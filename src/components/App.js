d44e8316f70864585cbcd96300f734c908447b5c
Fitzonreee <kevin.fitzhenry@createthenext.com>
2017-05-16 11:35:16 -0700

    Routed up the Teachers and Courses components in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -5,12 +5,20 @@ import {
 } from 'react-router-dom';
 
 // App components
+import Header from './Header';
 import Home from './Home';
+import About from './About';
+import Courses from './Courses';
+import Teachers from './Teachers';
 
 const App = () => (
   <BrowserRouter>
     <div className="container">
-      <Route path="/" component={Home} />
+      <Header />
+      <Route exact path="/" component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/teachers" component={Teachers} />
+      <Route path="/courses" component={Courses} />
     </div>
   </BrowserRouter>
 );
